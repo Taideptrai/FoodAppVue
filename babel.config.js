e7a7be8c3a573d45ae3befbf0ea4efee4d9e7047
TaiNguyen <nguyenvantaizzz@gmail.com>
2020-07-26 01:27:29 +0300

    update 4th

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
-};
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/FoodAppVue/' // Thay tên repository của các bạn vào đây nhé
+    : '/'
+}
